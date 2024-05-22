2ab4ef4224829b98721254955c76561f74d4ab30
tomaszkedziora14 <tomaszkedziora14@wp.pl>
2024-05-22 22:45:58 +0200

    fix

--- app/Http/Controllers/WinnersController.php
+++ app/Http/Controllers/WinnersController.php
@@ -19,18 +19,16 @@ class WinnersController extends Controller
         $winners = $this->winnersDrawsService->getWinners();
         return view('winners.index', ['winners' => $winners]);
     }
-    
+
     public function showWinningDrawsForLotteryName($lotteryName)
     {
         $winningDraws = $this->winnersDrawsService->getWinningDrawsForLotteryName($lotteryName);
-        dd($winningDraws);
-      //  return view('winners.index', ['winningDraws' => $winningDraws]);
+        return view('winners.index', ['winningDraws' => $winningDraws]);
     }
 
     public function showWinningDrawsForLotteryId($lotteryId)
     {
         $winningDraws = $this->winnersDrawsService->getWinningDrawsForLotteryId($lotteryId);
-        dd($winningDraws);
         return view('winners.index', ['winningDraws' => $winningDraws]);
     }
 }
